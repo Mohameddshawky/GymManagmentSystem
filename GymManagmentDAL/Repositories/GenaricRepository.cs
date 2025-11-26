b144c45de4d7258f96759608705548b26ae6442f
mohamed shawky <shawky1mohamed2@gmail.com>
2025-11-26 21:06:39 +0200

    Refactor GymDbcontext and add generic repository
    Refactored `GymDbcontext` to support dependency injection by replacing the `OnConfiguring` method with a constructor. Registered `GymDbcontext` in the DI container in `Program.cs` and moved the connection string to `appsettings.Development.json`.
    
    Added a generic repository pattern:
    - Implemented `GenaricRepository<T>` for CRUD operations.
    - Defined `IGenaricRepository<T>` interface for repository contracts.
    
    These changes improve maintainability, testability, and scalability of the codebase.

---
+++ GymManagmentDAL/Repositories/GenaricRepository.cs
@@ -0,0 +1,44 @@
+﻿using GymManagmentDAL.Data.Contexts;
+using GymManagmentDAL.Entites;
+using GymManagmentDAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagmentDAL.Repositories
+{
+    internal class GenaricRepository<T>(GymDbcontext context) : IGenaricRepository<T> where T : BaseEntity
+    {
+        public async Task AddAsync(T data)
+        {
+           await context.Set<T>().AddAsync(data);
+        }
+
+        public  void Delete(T data)
+        {
+            context.Set<T>().Remove(data);  
+
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+            => await context.Set<T>().ToListAsync();
+        
+
+        public async Task<T?> GetAsync(int id)
+            => await context.Set<T>().FindAsync(id);
+        
+
+        public async Task<int> SaveChangesAsync()
+            =>await context.SaveChangesAsync();
+
+        public Task<IEnumerable<T>> SearchAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T data)=>
+            context.Set<T>().Update(data);
+    }
+}
---
+++ GymManagmentDAL/Repositories/Interfaces/IGenaricRepository.cs
@@ -0,0 +1,19 @@
+﻿using GymManagmentDAL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagmentDAL.Repositories.Interfaces
+{
+    public interface IGenaricRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> SearchAsync(string name);
+
+        Task<T?> GetAsync(int id);
+        Task AddAsync(T data);
+        void Update(T data);
+        void Delete(T data);
+        Task<int> SaveChangesAsync();    
+    }
+}
