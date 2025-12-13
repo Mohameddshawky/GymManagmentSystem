e70c2b7f2cee5f85d1512b601611303f78f126aa
mohamed shawky <shawky1mohamed2@gmail.com>
2025-12-13 15:10:17 +0200

    Add database seeding for categories and plans at startup
    Introduce GymDbContextSeeding to populate initial categories and plans from JSON files if tables are empty. Update Program.cs to run migrations and seed data on app startup. Add categories.json and plans.json with default values to support automatic database initialization.

---
+++ GymManagmentDAL/Data/DataSeed/GymDbContextSeeding.cs
@@ -0,0 +1,64 @@
+﻿using GymManagmentDAL.Data.Contexts;
+using GymManagmentDAL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+
+namespace GymManagmentDAL.Data.DataSeed
+{
+    public static class GymDbContextSeeding
+    {
+        public static bool SeedData( GymDbcontext context)
+        {
+            try
+            {
+                var hasPlans = context.plans.Any();
+                var hasCategories = context.categories.Any();
+
+                if (hasPlans && hasCategories) return false;
+
+                if (!hasCategories)
+                {
+                    var categories = LoadDataFromJson<Category>("categories.json");
+                    if (categories.Any())
+                    {
+                        context.categories.AddRange(categories);
+                    }
+
+                }
+                if (!hasPlans)
+                {
+                    var plans = LoadDataFromJson<Plan>("plans.json");
+                    if (plans.Any())
+                    {
+                        context.plans.AddRange(plans);
+                    }
+                }
+                return context.SaveChanges() > 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Seeding Failed {ex.Message}");
+                return false;
+            }
+        }
+
+
+        private static List<T> LoadDataFromJson<T>(string fileName) 
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files",fileName);
+            if (!File.Exists(filePath)) throw new FileNotFoundException($"The file {fileName} was not found at path {filePath}.");
+
+            string data = File.ReadAllText(filePath);
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            return JsonSerializer.Deserialize<List<T>>(data, options) ?? new List<T>();
+        }
+    }
+}
--- GymManagmentPL/Program.cs
+++ GymManagmentPL/Program.cs
@@ -3,6 +3,7 @@ using GymManagmentDAL.Repositories.Interfaces;
 using GymManagmentDAL.Repositories;
 using GymManagmentBLL.Mapping;
 using Microsoft.EntityFrameworkCore;
+using GymManagmentDAL.Data.DataSeed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,19 @@ builder.Services.AddAutoMapper(m => m.AddProfile(new HealthRecordProfile()));
 builder.Services.AddAutoMapper(m => m.AddProfile(new PlanProfile()));
 
 var app = builder.Build();
+
+// Seed Data
+ using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<GymDbcontext>();
+    var pendingMigrations = context.Database.GetPendingMigrations();
+    if (pendingMigrations?.Any()??false)
+    {
+        context.Database.Migrate();
+    }
+    GymDbContextSeeding.SeedData(context);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
