23f32e652e36df4251cb6aca2c78d627a47ef892
mohamed shawky <shawky1mohamed2@gmail.com>
2026-02-07 14:06:35 +0200

    Add Membership controller and index view

---
+++ GymManagmentPL/Controllers/MemberShipController.cs
@@ -0,0 +1,21 @@
+﻿using GymManagmentBLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GymManagmentPL.Controllers
+{
+    public class MemberShipController : Controller
+    {
+        private readonly IMemberShipService memberShipService;
+
+        public MemberShipController(IMemberShipService memberShipService)
+        {
+            this.memberShipService = memberShipService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var memberShips =await memberShipService.GetAllMemberShipAsync();
+            return View(memberShips);
+        }
+    }
+}
